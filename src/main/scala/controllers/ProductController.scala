bcbd2e2472bd4acff1c0fc4999610d7ea0377c44
Sushant Somani <somanisushant23@gmail.com>
2024-03-06 11:20:44 +0530

    refactor

--- src/main/scala/controllers/ProductController.scala
+++ src/main/scala/controllers/ProductController.scala
@@ -1,16 +1,10 @@
 package controllers
 
-import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.stream.ActorMaterializer
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import services.{FakeStoreProductsService, ProductService}
+import services.ProductService
 
 trait ProductController extends ProductService {
-  /*implicit val system       = ActorSystem("ProductControllerActor")
-  implicit val materializer = ActorMaterializer()(system)
-  import system.dispatcher*/
 
   val productsRoutePath = "products"
   val hostName          = "localhost"
@@ -38,5 +32,4 @@ trait ProductController extends ProductService {
       }
     }
   }
-  //Http().bindAndHandle(productsRoute, hostName, hostPort)
 }

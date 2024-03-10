79c7af6c604aa4646097dfdef2808f3672aa44db
Sushant Somani <somanisushant23@gmail.com>
2024-03-10 21:30:52 +0530

    Adding validations

--- src/main/scala/controllers/ProductController.scala
+++ src/main/scala/controllers/ProductController.scala
@@ -2,19 +2,27 @@ package controllers
 
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.StatusCodes
+import model.ProductServiceEntities.Product
 import services.ProductService
+import model.JsonProtocol._
 
 trait ProductController extends ProductService {
-
   val productsRoutePath = "products"
+
   val productsRoute = {
     path(productsRoutePath) {
       get {
         complete(getAllProducts())
+      } ~ post {
+        entity(as[Product]) { product =>
+          println(s"Product received is $product")
+          complete(StatusCodes.OK)
+        }
       }
     } ~
       path(productsRoutePath / LongNumber) { id =>
-        complete(getProductDetails(id.toLong))
+        complete(getProductDetails(id))
       }
   }
 
